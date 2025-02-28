36c5353fccbeb8cfe15e06f64d4ea73167945dc0
asima295 <ftasleem581@gmail.com>
2025-02-28 12:07:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+import { multiply } from "./style.js";
+
+const result = multiply(2, 3, 4);
+
+document.getElementById("demo").innerHTML = result;
+
+
+
+
+import { hello } from "./style.js";
+
+const result1 = hello(2, 3, 4);
+
+document.getElementById("demo1").innerHTML = result1;
+
+
+
+import { me } from "./style.js";
+
+const result2 = me(2, 3, 4);
+
+document.getElementById("demo2").innerHTML = result2;
+
+
+
+import { name } from "./style.js";
+
+const result3 = name(2, 3, 4);
+
+document.getElementById("demo3").innerHTML = result3;
+
+
+
+
+import { word } from "./style.js";
+
+const result4 = name(2, 3, 4);
+
+document.getElementById("demo3").innerHTML = result3;
+
