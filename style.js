ac566e9d9ddcbbfac08dd0fc6425e71b77a37e06
asima295 <ftasleem581@gmail.com>
2025-02-28 12:07:34 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,27 @@
+export function multiply(a, b, c) {
+    return a * b * c; 
+}
+
+
+
+export function hello(a, b, c) {
+    return a + b + c; 
+}
+
+
+
+export function me(a, b, c) {
+    return a - b - c; 
+}
+
+
+
+export function name(a, b, c) {
+    return a - b + c; 
+}
+
+
+
+export function word(a, b, c) {
+    return a - b + c; 
+}
